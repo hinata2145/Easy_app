8f6dded26159b05ec5b256f7cc924ae37c6ec019
hinata2145 <hinatamaru2145@gmail.com>
2024-02-07 21:14:37 +0900

    画像を表示できるように変更した。

--- lib/component/common_button.dart
+++ lib/component/common_button.dart
@@ -6,25 +6,37 @@ class CommonButton extends StatelessWidget {
       required this.text,
       this.onpressed,
       this.textcolor,
-      this.backgroundcolor});
+      this.backgroundcolor,
+      this.image});
 
   final String text;
   final void Function()? onpressed;
   final Color? textcolor;
   final Color? backgroundcolor;
+  final String? image;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
+        side: const BorderSide(
+          color: Colors.black,
+          width: 1,
+        ),
         foregroundColor: textcolor ?? Colors.white,
         backgroundColor: backgroundcolor ?? Colors.black,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       ),
       onPressed: onpressed,
-      child: Text(
-        text,
-        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          image != null ? Image.asset(image!) : Container(),
+          Text(
+            text,
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+        ],
       ),
     );
   }
