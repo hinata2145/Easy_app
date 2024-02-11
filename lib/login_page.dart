5a7a7e74d494cdd734843ab5a057ac738fb00194
hinata2145 <hinatamaru2145@gmail.com>
2024-02-11 17:22:28 +0900

    デザインの変更

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,136 +10,34 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 270,
-                  child: Column(
-                    children: [
-                      const Text(
-                        "EasyAppを始めよう!",
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      const SizedBox(height: 48),
-                      const TextField(
-                        decoration: InputDecoration(
-                          labelStyle: TextStyle(fontSize: 20),
-                          labelText: 'メールアドレス',
-                          floatingLabelBehavior: FloatingLabelBehavior.always,
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      const TextField(
-                        decoration: InputDecoration(
-                          labelStyle: TextStyle(fontSize: 20),
-                          labelText: 'パスワード',
-                          floatingLabelBehavior: FloatingLabelBehavior.always,
-                        ),
-                      ),
-                      const SizedBox(height: 48),
-                      SizedBox(
-                        width: 270,
-                        height: 40,
-                        child: CommonButton(
-                          text: 'ログイン',
-                          onpressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const HomePage()),
-                            );
-                          },
-                          ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 48),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: Container(
-                          height: 1,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 14),
-                        child: Text(
-                          'or',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          height: 1,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  width: 270,
+      appBar: AppBar(
+        title: const Text("ログイン"),
+      ),
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.only(top: 40),
+          child: Column(
+            children: [
+              SizedBox(
                   height: 40,
+                  width: 340,
                   child: SignInButton(
-                      Buttons.google,
-                      text: "Googleでログイン",
-                      onPressed: () {},
-                    )
-                ),
-                const SizedBox(
-                  height: 12,
-                ),
-                SizedBox(
-                  width: 270,
+                    Buttons.google,
+                    text: "Googleで続行",
+                    onPressed: () {},
+                  )),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
                   height: 40,
-                  child:  SignInButton(
-                      Buttons.apple,
-                      text: "appleでログイン",
-                      onPressed: () {},
-                    )
-                ),
-                const SizedBox(
-                  height: 12,
-                ),
-                SizedBox(
-                  width: 270,
-                  height: 40,
-                  child: CommonButton(
-                    text: '新規登録',
-                    onpressed: () {},
-                  ),
-                ),
-                const SizedBox(
-                  height: 48,
-                ),
-                RichText(
-                  text: TextSpan(
-                    text: "ご利用規約",
-                    style: const TextStyle(color: Colors.black, fontSize: 12),
-                    recognizer: TapGestureRecognizer()..onTap = () {},
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                RichText(
-                  text: TextSpan(
-                    text: "プライバシーポリシー",
-                    style: const TextStyle(color: Colors.black, fontSize: 12),
-                    recognizer: TapGestureRecognizer()..onTap = () {},
-                  ),
-                ),
-              ],
-            ),
+                  width: 340,
+                  child: SignInButton(
+                    Buttons.apple,
+                    text: "Appleで続行",
+                    onPressed: () {},
+                  )),
+            ],
           ),
         ),
       ),
