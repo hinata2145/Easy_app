8d291dd7d08bee21ce7f09f26f57f55ec6b44fd0
hinata2145 <hinatamaru2145@gmail.com>
2024-02-11 16:02:55 +0900

    アカウント登録画面の追加

---
+++ lib/first_page.dart
@@ -0,0 +1,83 @@
+import 'package:easy_app/component/common_button.dart';
+import 'package:easy_app/login_page.dart';
+import 'package:easy_app/signup_page.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 270,
+                height: 40,
+                child: CommonButton(
+                  text: '新規登録',
+                  onpressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SignupPage()),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 36,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 36),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 14),
+                      child: Text(
+                        'アカウントをお持ちの方',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 12),
+              RichText(
+                text: TextSpan(
+                  text: "ログイン",
+                  style: const TextStyle(color: Colors.blue, fontSize: 16),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LoginPage()),
+                      );
+                    },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
