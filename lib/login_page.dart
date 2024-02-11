7b21b4680227ade6ce86c565d5671ebd7abf40f3
hinata2145 <hinatamaru2145@gmail.com>
2024-02-11 17:55:52 +0900

    ボタンと入力フォームの追加

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:easy_app/component/common_button.dart';
+import 'package:easy_app/component/input_form.dart';
 import 'package:easy_app/home_page.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -30,13 +31,83 @@ class LoginPage extends StatelessWidget {
                 height: 20,
               ),
               SizedBox(
-                  height: 40,
-                  width: 340,
-                  child: SignInButton(
-                    Buttons.apple,
-                    text: "Appleで続行",
-                    onPressed: () {},
-                  )),
+                height: 40,
+                width: 340,
+                child: SignInButton(
+                  Buttons.apple,
+                  text: "Appleで続行",
+                  onPressed: () {},
+                ),
+              ),
+              SizedBox(height: 48),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 36),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            height: 1,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 14),
+                          child: Text(
+                            'or',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            height: 1,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 48),
+                  Text('メールアドレス'),
+                  SizedBox(height: 12),
+                  InputForm(),
+                  SizedBox(height: 12),
+                  Text('パスワード'),
+                  SizedBox(height: 12),
+                  InputForm(),
+                  SizedBox(height: 60),
+                  CommonButton(
+                    text: 'ログイン',
+                    onpressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomePage()),
+                      );
+                    },
+                  ),
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      text: "パスワードの再設定",
+                      style: const TextStyle(color: Colors.blue, fontSize: 16),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const LoginPage()),
+                          );
+                        },
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
